e7bf9976d9eecd9122aa7861c239c329bcf4b197
Jefrey Bulla <jefrey.bulla@gmail.com>
2018-10-06 21:34:36 -0700

    api conversion working for one currency

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Dashboard from '@/components/Dashboard'
 import Signup from '@/components/Signup'
+import Conversion from '@/components/Conversion'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/signup',
       name: 'signup',
       component: Signup
+    },
+    {
+      path: '/conversion',
+      name: 'conversion',
+      component: Conversion
     }
   ]
 })
